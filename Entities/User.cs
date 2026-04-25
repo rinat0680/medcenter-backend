902f67956b261afad7a8ab35e0b058544173a140
RamazonFarmanov <romafarmanov@gmail.com>
2026-04-26 02:28:10 +0500

    feature: user entity and solution added

---
+++ Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalCenterApi.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Username { get; set; }
+    public string Password { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public bool IsAdmin { get; set; }
+}
