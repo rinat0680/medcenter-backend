921faada83342de703c241101b32caa79419a10d
rinat0680 <998930850680@gmail.com>
2026-04-29 02:00:12 +0500

    Created AuthController, LoginRequestDto, RegisterRequestDto

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -47,5 +47,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 app.Run();
 
