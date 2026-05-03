b5eb29790a6e1759f43ddb7c20b0f24ed52dc704
rinat0680 <998930850680@gmail.com>
2026-05-03 13:47:29 +0500

    DI in AuthService: hasher&tokenService

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -4,5 +4,11 @@ namespace MedicalCenterApi;
 
 public class AuthService : IAuthService
 {
-
+    private readonly IPasswordHasher _hasher;
+    private readonly ITokenService _tokenService;
+    public AuthService(IPasswordHasher hasher, ITokenService tokenService)
+    {
+        _hasher = hasher;
+        _tokenService = tokenService;
+    }
 }
