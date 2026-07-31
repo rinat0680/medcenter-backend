7467b052a250d7c4cebb1a5b433b4deff828393a
rinatraev <64296176+rinatraev@users.noreply.github.com>
2026-07-31 10:00:36 +0500

    Service Requests handler created and registere in DI container as Scoped

---
+++ ServiceDomain/ServiceRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalCenterApi;
+
+public class ServiceRequestHandler
+{
+    private readonly AppDbContext _dbContext;
+    public ServiceRequestHandler(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    
+}
